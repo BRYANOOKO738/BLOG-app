8e0aaf274188b0d6491820299b64d9902aec3bfc
Bryan Onyango <onyangobryan8@gmail.com>
2024-09-24 18:03:39 -0700

    redux

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import Store from './redux/Store.js';
+import store, { persistor } from "./redux/Store.js"; // Corrected import
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={Store}>
-      <App />
-    </Provider>   
-  </BrowserRouter>
+  <StrictMode>
+    <Provider store={store}>
+      {" "}
+      {/* 'store' from default import */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 );
--- Client/src/redux/Store.js
+++ Client/src/redux/Store.js
@@ -1,10 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { userReducer } from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // This uses localStorage
+import { persistStore } from 'redux-persist'; // Correct import here
 
-const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
+const rootReducer = combineReducers({
+    user: userReducer,
 });
 
-export default store; // Change to named export if needed
+const persistConfig = {
+    key: 'root',
+    storage: storage, // Use default localStorage for persistence
+    version: 1,
+};
+
+// Wrap the rootReducer with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false, // Disable serializable checks for large state
+    }),
+});
+
+// Create the persistor
+export const persistor = persistStore(store);
+
+// Export the store as default
+export default store;
