13599d3d0d2db7773f57b0c9f3b56e928e3cbfdd
Bryan Onyango <onyangobryan8@gmail.com>
2024-10-04 17:29:55 -0700

    upload functionality

--- Client/src/Pages/Create_post.jsx
+++ Client/src/Pages/Create_post.jsx
@@ -1,29 +1,89 @@
-import React from 'react'
-import "../Components/Create_post.css"
+import React, { useState } from "react";
+import "../Components/Create_post.css";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "../Components/Navbar/Navbar.css";
+ import { ToastContainer, toast } from "react-toastify";
+ import "react-toastify/dist/ReactToastify.css";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import {app} from "../Firebase";
 
 const Create_post = () => {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [file, setFile] = useState(null);
+  const [formData, setFormData] = useState({});
+  
+
+  const handleImageUpload = () => {
+    try {
+      if (!file) {
+        setImageUploadError("Please select an image file");
+        return;
+      }
+      const storage = getStorage(app);
+      const filename = new Date().getTime() + "-" + file.name;
+      const storageRef = ref(storage, filename);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+          setImageUpload(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError("Failed to upload");
+          setImageUpload(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUpload(null);
+            setImageUploadError(null);
+            setFormData((prevData) => ({ ...prevData, image: downloadURL }));
+          });
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      setImageUploadError("Failed to upload");
+      setImageUpload(null);
+    }
+  };
+
   return (
-    <div class="container-fluid">
-      <div class="row justify-content-center">
-        <div class="col-12 col-lg-8 col-xl-6">
-          <div class="pt-3 px-3 px-md-4">
-            <h2 class="text-center fw-bold mb-4">Create a post</h2>
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-12 col-lg-8 col-xl-6">
+          <div className="pt-3 px-3 px-md-4">
+            <h2 className="text-center fw-bold mb-4">Create a post</h2>
             <form>
-              <div class="row g-3">
-                <div class="col-md-8">
-                  <label for="title" class="form-label">
+              <div className="row g-3">
+                <div className="col-md-8">
+                  <label htmlFor="title" className="form-label">
                     Title
                   </label>
-                  <input type="text" class="form-control" id="title" required />
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    required
+                  />
                 </div>
-                <div class="col-md-4">
-                  <label for="category" class="form-label">
+                <div className="col-md-4">
+                  <label htmlFor="category" className="form-label">
                     Category
                   </label>
-                  <select class="form-select" id="category">
+                  <select className="form-select" id="category">
                     <option value="" disabled selected>
                       Select the Category
                     </option>
@@ -32,25 +92,52 @@ const Create_post = () => {
                     <option value="quality">Quality</option>
                   </select>
                 </div>
-                <div class="col-12">
-                  <div class="border border-2 border-primary p-3 rounded">
-                    <div class="d-flex justify-content-between align-items-center">
+                <div className="col-12">
+                  <h4>Upload an image</h4>
+                  <div className="border border-2 border-primary p-3 rounded">
+                    <div className="d-flex justify-content-between align-items-center">
                       <input
                         type="file"
                         accept="image/*"
-                        class="form-control"
+                        className="form-control"
                         id="image-upload"
+                        onChange={(e) => setFile(e.target.files[0])}
                       />
                       <button
                         type="button"
-                        class="btn btn-outline-success ms-2"
+                        className="btn btn-outline-success ms-2"
+                        onClick={handleImageUpload} // Attach image upload logic here
+                        disabled={imageUpload > 0 && imageUpload < 100}
                       >
-                        Upload
+                        {imageUpload ? (
+                          <div style={{ width: "30px", height: "30px" }}>
+                            <CircularProgressbar
+                              value={imageUpload}
+                              text={`${imageUpload}%`}
+                              styles={{
+                                path: { stroke: "#4db8ff" }, // Customize stroke color if needed
+                                text: { fontSize: "10px", fill: "#4db8ff" }, // Customize text style if needed
+                              }}
+                            />
+                          </div>
+                        ) : (
+                          "Upload"
+                        )}
                       </button>
                     </div>
                   </div>
                 </div>
-                <div style={{ position: "relative", height: "200px" }}>
+                {formData.image && (
+                  <div className="text-center w-100%">
+                    <img
+                      src={formData.image}
+                      alt="Uploaded Image"
+                      style={{ Width: "100%", maxHeight: "200px" }}
+                      className="w-100%"
+                    />
+                  </div>
+                )}
+                <div style={{ position: "relative", height: "230px" }}>
                   <ReactQuill
                     theme="snow"
                     className="mb-1 h-50"
@@ -60,10 +147,9 @@ const Create_post = () => {
                 </div>
                 <div className="d-flex justify-content-center mb-3 ">
                   <button
-                    
                     type="submit"
                     className="btn  logo"
-                    style={{width:"40%"}}
+                    style={{ width: "40%" }}
                   >
                     Publish Post
                   </button>
@@ -75,6 +161,6 @@ const Create_post = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Create_post
+export default Create_post;
